39e8394bcc4bf6ae66354b85601775838f63e9bc
TaskXD <105731310+TaskXD@users.noreply.github.com>
2023-08-05 00:02:09 +0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -5,21 +5,20 @@ import mysql.connector
 import pickle
 import re
 import datetime
-from google.cloud.sql.connector import connector 
 import sqlalchemy
 
-INSTANCE_CONNECTION_NAME = f"{prime-bridge-394911}:{us-central1}:{task}"
-
+INSTANCE_CONNECTION_NAME = "[YOUR_INSTANCE_CONNECTION_NAME]"
 DB_USER = "root"
 DB_PASS = "123456"
 DB_NAME = "hdb"
 
-
 # Load the best model from the pickle file
+# (Make sure to provide the correct file path for your 'best_model.pkl' file)
 with open('best_model.pkl', 'rb') as file:
     xgb_model = pickle.load(file)
 
 # Load the pickles for scaler, PCA, and label encoders
+# (Make sure to provide the correct file paths for your pickle files)
 with open('scaler.pkl', 'rb') as file:
     scaler = pickle.load(file)
 
@@ -34,14 +33,13 @@ with open('LABEL_TYPE_label_encoder.pkl', 'rb') as file:
 
 # Create a function to preprocess the input data
 def preprocess_input_data(data):
-
     # Handle VEHICLETYPE column (if it has three options 'C', 'M', and 'E')
     data['VEHICLETYPE'] = vehicletype_label_encoder.transform(data['VEHICLETYPE'])
 
     # Handle TOTAL_CHARGE column (if it's numeric)
     data['TOTAL_CHARGE'] = data['TOTAL_CHARGE'].astype(float)
 
-    # Handle durration column (if it's numeric)
+    # Handle duration column (if it's numeric)
     data['DURATION'] = data['DURATION'].astype(float)
 
     # Apply the loaded scaler to scale the input features
@@ -62,25 +60,21 @@ def predict_label_type(features):
     return original_predictions
 
 # Function to create a MySQL connection
-#initialize Connector object
-connector = Connector()
-
-#function to return the database connection object 
 def getconn():
-    conn = connector.connect(
-        INSTANCE_CONNECTION_NAME,
-        "pymysql",
+    return mysql.connector.connect(
+        host=INSTANCE_CONNECTION_NAME,
         user=DB_USER,
         password=DB_PASS,
-        db=DB_NAME )
-    return conn
+        database=DB_NAME
+    )
 
-#create connection pool with 'creator' arguement to our connection object function 
+# Create connection pool with 'creator' argument to our connection object function
 pool = sqlalchemy.create_engine(
     "mysql+pymysql://",
-creator=getconn,)
+    creator=getconn,
+)
 
-#Function to insert parking data in Database
+# Function to insert parking data in Database
 def insert_parking_details(user_id, vehicle_type, predicted_label, lot_no, duration, total_charge):
     connection = create_connection()
     if connection is not None:
@@ -103,8 +97,6 @@ def insert_parking_details(user_id, vehicle_type, predicted_label, lot_no, durat
             # Return the session start timestamp
             return session_start
 
-
-
 # Function to validate email
 def is_valid_email(email):
     email_pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
@@ -125,7 +117,6 @@ def is_valid_account_number(number):
     return re.match(number_pattern, number)
 
 def show_parking_details(user_id):
-
     connection = create_connection()
     if connection is not None:
         cursor = connection.cursor()
@@ -139,14 +130,14 @@ def show_parking_details(user_id):
             st.write('User ID:', parking_details[0])
             st.write('Vehicle:', parking_details[1])
             st.write('Predicted Label:', parking_details[2])
-            st.write('Alloted lot no :', parking_details[3])
-            st.write('DURATION :', parking_details[4])
-            st.write('Seesion started at :', parking_details[5])
+            st.write('Allotted lot no:', parking_details[3])
+            st.write('Duration:', parking_details[4])
+            st.write('Session started at:', parking_details[5])
             st.write('Total Charge:', parking_details[6])
         else:
             st.warning("No parking details found.")
 
-#function to check capacity of parking lots
+# Function to check parking capacity
 def check_parking_capacity():
     connection = create_connection()
     if connection is not None:
@@ -168,11 +159,10 @@ def check_parking_capacity():
         percentage = (total_allotted / (total_allotted + total_not_allotted)) * 100
 
         st.subheader('Parking Capacity Check')
-        # st.write(f'Not Allotted Lot Numbers: {", ".join(str(lot) for lot in sorted(not_allotted_lots))}')
-        st.write(f'{percentage:.2f}%   of Parking Lot is currently occupied. ')
+        st.write(f'{percentage:.2f}% of Parking Lot is currently occupied.')
         st.write(f'Allotted Lot Numbers: {", ".join(str(lot) for lot in sorted(allotted_lots))}')
 
-#get a lot number after checking database
+# Function to get a lot number after checking the database
 def get_lot_number(range_option):
     connection = create_connection()
     if connection is not None:
@@ -197,8 +187,7 @@ def get_lot_number(range_option):
             if lot_no not in alloted_lots:
                 return lot_no
 
-#Function to report parking
-
+# Function to report parking
 def report_parking(user_id, lot_no, vehicle_type, predicted_label, description):
     connection = create_connection()
     if connection is not None:
@@ -210,7 +199,7 @@ def report_parking(user_id, lot_no, vehicle_type, predicted_label, description):
         connection.close()
         st.success('Parking report submitted successfully!')
 
-#check whether already reported or not
+# Function to check whether already reported or not
 def check_existing_report(user_id):
     connection = create_connection()
     if connection is not None:
