ec807452b7d4512e961a5cf5edbbe9c066dc213d
TaskXD <105731310+TaskXD@users.noreply.github.com>
2023-08-06 19:54:42 +0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -397,22 +397,27 @@ def main():
 
 
         st.markdown('<h3 style="color: #e74c3c;">Report Parking</h3>', unsafe_allow_html=True)
-
+        
         with st.expander('Report Parking', expanded=False):
             lot_no = st.selectbox('Select parked car lot number', list(range(1, 501)))
             vehicle_type = st.selectbox('Select parked car vehicle type', ['C', 'M', 'E'])
             predicted_label = st.selectbox('Predicted Label', ['SHORT TERM', 'Season_W'])
             description = st.text_area('Describe the issue (optional)', height=100)
-            
-        with st.button('Submit Report'):
-            report_parking(user_details['user_id'], lot_no, vehicle_type, predicted_label, description)
-                    # Clear the fields after submitting the report
-                    lot_no = None
-                    vehicle_type = None
-                    predicted_label = ""
-                    description = ""
-                    st.success('Parking report submitted successfully!')
-            
+
+            if st.button('Submit Report'):
+                report_parking(user_details['user_id'], lot_no, vehicle_type, predicted_label, description)
+                # Clear the fields after submitting the report
+                vehicle_type = None
+                predicted_label = ""
+                description = ""
+                st.success('Parking report submitted successfully!')
+            else: 
+                # Clear the fields after submitting the report
+                vehicle_type = None
+                predicted_label = ""
+                description = ""
+                st.success('Parking report submitted successfully!')
+                
     else:
         st.empty()
         st.empty()
